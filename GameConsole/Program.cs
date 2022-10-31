4750c021a66fbc9306fde71783a74a70eaecad4e
IvanTushinski <tushinski.ivan@gmail.com>
2022-10-31 17:01:59 +0300

    Добавьте файлы проекта.

---
+++ GameConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using GameConsole;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        SaveLoad saveLoad = new SaveLoad();
+
+
+
+        int i = 1;
+        while (i > 0)
+        {
+            Console.WriteLine("Press 1(Save) or 2(Load) or 3");
+            String selectNumbers;
+            selectNumbers = Console.ReadLine();
+            if (selectNumbers == "1")
+            {
+                saveLoad.saveData();
+            }
+            else if (selectNumbers == "2")
+            {
+                saveLoad.loadData();
+            }
+            else if (selectNumbers == "3")
+            {
+                break;
+            }
+            i++;
+        }
+    }
+}
---
+++ GameConsole/SaveLoad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    public class SaveLoad
+    {
+        public void saveData()
+        {
+            StreamWriter sw = new StreamWriter("C:\\Users\\user\\source\\repos\\GameConsole\\GameConsole\\saveData.txt");
+            //Write a line of text
+            int a = 12;
+            sw.Write(a);
+            sw.Close();
+        }
+
+        public void loadData()
+        {
+            StreamReader streamReader = new StreamReader("C:\\Users\\user\\source\\repos\\GameConsole\\GameConsole\\saveData.txt");
+            String line = streamReader.ReadLine();
+            int a = int.Parse(line);
+            Console.WriteLine("Saved Data is " + a);
+            streamReader.Close();
+            Console.ReadLine();
+        }
+    }
+}
